bb4fd0c6c0bb07c47f6861ccb719c118bdb8011e
jeffreyevan <jeffrey_nave_evan@live.com>
2020-02-20 10:59:36 +0700

    Update LearningXml.java

--- Learning Xml/src/learning/xml/LearningXml.java
+++ Learning Xml/src/learning/xml/LearningXml.java
@@ -6,11 +6,11 @@
 package learning.xml;
 
 import java.io.File;
-import javax.xml.parsers.DocumentBuilderFactory;  
-import javax.xml.parsers.DocumentBuilder;  
-import org.w3c.dom.Document;  
-import org.w3c.dom.NodeList;  
-import org.w3c.dom.Node;  
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Node;
 import org.w3c.dom.Element;
 /**
  *
@@ -25,22 +25,22 @@ public class LearningXml {
         try {
             File xmlFile = new File("src/learning/xml/Student.xml");
             DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-            DocumentBuilder db = dbf.newDocumentBuilder();  
-            Document doc = db.parse(xmlFile);  
+            DocumentBuilder db = dbf.newDocumentBuilder();
+            Document doc = db.parse(xmlFile);
             doc.getDocumentElement().normalize();
-            System.out.println("Root element: " + doc.getDocumentElement().getNodeName());  
+            System.out.println("Root element: " + doc.getDocumentElement().getNodeName());
             NodeList nodeList = doc.getElementsByTagName("student");
-            for (int itr = 0; itr < nodeList.getLength(); itr++) {  
-                Node node = nodeList.item(itr);  
-                System.out.println("\nNode Name :" + node.getNodeName());  
-                if (node.getNodeType() == Node.ELEMENT_NODE) {  
-                    Element eElement = (Element) node;  
-                    System.out.println("Student id: "+ eElement.getElementsByTagName("id").item(0).getTextContent());  
-                    System.out.println("First Name: "+ eElement.getElementsByTagName("firstname").item(0).getTextContent());  
-                    System.out.println("Last Name: "+ eElement.getElementsByTagName("lastname").item(0).getTextContent());  
-                    System.out.println("Subject: "+ eElement.getElementsByTagName("subject").item(0).getTextContent());  
-                    System.out.println("Marks: "+ eElement.getElementsByTagName("marks").item(0).getTextContent());  
-                }  
+            for (int i = 0; i < nodeList.getLength(); i++) {
+                Node node = nodeList.item(i);
+                System.out.println("\nNode Name :" + node.getNodeName());
+                if (node.getNodeType() == Node.ELEMENT_NODE) {
+                    Element eElement = (Element) node;
+                    System.out.println("Student id: "+ eElement.getElementsByTagName("id").item(0).getTextContent());
+                    System.out.println("First Name: "+ eElement.getElementsByTagName("firstname").item(0).getTextContent());
+                    System.out.println("Last Name: "+ eElement.getElementsByTagName("lastname").item(0).getTextContent());
+                    System.out.println("Subject: "+ eElement.getElementsByTagName("subject").item(0).getTextContent());
+                    System.out.println("Marks: "+ eElement.getElementsByTagName("marks").item(0).getTextContent());
+                }
             }
         }
         catch(Exception e) {
