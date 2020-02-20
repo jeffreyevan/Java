64f5da1e9f03ca75133d4f8e113c2a5d42dde79b
jeffreyevan <jeffrey_nave_evan@live.com>
2020-02-20 10:50:30 +0700

    XML Parsing using DOM

---
+++ Learning Xml/src/learning/xml/LearningXml.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package learning.xml;
+
+import java.io.File;
+import javax.xml.parsers.DocumentBuilderFactory;  
+import javax.xml.parsers.DocumentBuilder;  
+import org.w3c.dom.Document;  
+import org.w3c.dom.NodeList;  
+import org.w3c.dom.Node;  
+import org.w3c.dom.Element;
+/**
+ *
+ * @author jeffrey evan
+ */
+public class LearningXml {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        try {
+            File xmlFile = new File("src/learning/xml/Student.xml");
+            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+            DocumentBuilder db = dbf.newDocumentBuilder();  
+            Document doc = db.parse(xmlFile);  
+            doc.getDocumentElement().normalize();
+            System.out.println("Root element: " + doc.getDocumentElement().getNodeName());  
+            NodeList nodeList = doc.getElementsByTagName("student");
+            for (int itr = 0; itr < nodeList.getLength(); itr++) {  
+                Node node = nodeList.item(itr);  
+                System.out.println("\nNode Name :" + node.getNodeName());  
+                if (node.getNodeType() == Node.ELEMENT_NODE) {  
+                    Element eElement = (Element) node;  
+                    System.out.println("Student id: "+ eElement.getElementsByTagName("id").item(0).getTextContent());  
+                    System.out.println("First Name: "+ eElement.getElementsByTagName("firstname").item(0).getTextContent());  
+                    System.out.println("Last Name: "+ eElement.getElementsByTagName("lastname").item(0).getTextContent());  
+                    System.out.println("Subject: "+ eElement.getElementsByTagName("subject").item(0).getTextContent());  
+                    System.out.println("Marks: "+ eElement.getElementsByTagName("marks").item(0).getTextContent());  
+                }  
+            }
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
