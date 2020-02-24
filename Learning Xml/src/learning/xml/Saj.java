b1bf315114bec04e96bfdb0875a5a19919cf49b5
jeffreyevan <jeffrey_nave_evan@live.com>
2020-02-24 12:26:43 +0700

    update

--- Learning Xml/src/learning/xml/Saj.java
+++ Learning Xml/src/learning/xml/Saj.java
@@ -5,47 +5,28 @@
  */
 package learning.xml;
 
-import java.io.FileNotFoundException;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import java.io.FileReader;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
+
 /**
  *
  * @author jeffrey evan
  */
-public class Saj {
-    public static void main(String args[]) {
-        JSONParser jsonParser = new JSONParser();
-        try (FileReader reader = new FileReader("Student.json")) {
-            Object obj = jsonParser.parse(reader);
-            JSONArray studentList = (JSONArray) obj;
-            System.out.println(studentList);
-            studentList.forEach( std -> parseStudentObject( (JSONObject) std ) );
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
+public class SAJ {
+    public static void main(String args[]) throws FileNotFoundException, IOException {
+
+    //JSON parser object to parse read file
+    FileReader fr = new FileReader("src/learning/xml/Student.json");
+    JsonParser parser = new JsonParser();
+    JsonElement jsonTree = parser.parse(fr);
+
+    JsonObject jsonObject = jsonTree.getAsJsonObject();
+    JsonElement A = jsonObject.get("Class");
+    System.out.println(A);
     }
-
-    private static void parseStudentObject(JSONObject student) {
-      JSONObject studentObject = (JSONObject) student.get("student");
-      String firstName = (String) studentObject.get("firstName");
-      System.out.println(firstName);
-
-      String lastName = (String) studentObject.get("lastName");
-      System.out.println(lastName);
-
-      String subject = (String) studentObject.get("subject");
-      System.out.println(subject);
-
-      String marks = (String) studentObject.get("marks");
-      System.out.println(marks);
-    }
-  }
+}
